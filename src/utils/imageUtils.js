b9bd5fdd6115343a40c20b5b88fc6baf0d57772b
DEv2405LM-PHP <taomalai987@gmail.com>
2026-06-01 20:36:05 +0700

    main

--- src/utils/imageUtils.js
+++
@@ -1,8 +0,0 @@
-export const isImageSource = (value) =>
-  typeof value === 'string' && /^(https?:\/\/|\/|data:image\/|blob:)/i.test(value);
-
-export const getProductThumbnail = (product, fallback = '🧸') => (
-  (product?.images || []).find((image) => image.thumbnail)?.imageUrl ||
-  product?.images?.[0]?.imageUrl ||
-  fallback
-);
